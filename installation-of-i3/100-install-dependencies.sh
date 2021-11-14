9fe2275c9b8146e21fa106af14a8a0b5666e68d5
Erik Dubois <erik.dubois@gmail.com>
2021-11-14 12:16:46 +0100

    thunar as filemanager

--- installation-of-i3/100-install-dependencies.sh
+++ installation-of-i3/100-install-dependencies.sh
@@ -29,4 +29,26 @@ echo
 echo "#########################################"
 echo "dependencies for i3 with gaps installed"
 echo "#########################################"
-sleep 1
+
+
+# echo "###############################"
+# echo "Xcb-util-xrm"
+# echo "###############################"
+
+# dependancy on xcb-util-xrm
+
+# rm -rf /tmp/xcb-util-xrm
+# git clone --recursive https://github.com/Airblader/xcb-util-xrm.git /tmp/xcb-util-xrm
+# cd /tmp/xcb-util-xrm
+# git submodule update --init
+# sh /tmp/xcb-util-xrm/autogen.sh --prefix=/usr
+# make && sudo make install
+
+# rm -rf /tmp/xcb-util-xrm
+
+# echo
+# echo
+# echo "###############################"
+# echo "Xcb-util-xrm installed"
+# echo "###############################"
+# sleep 3
