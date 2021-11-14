4a7d4a048b72d472f36ef15bdc53e9864b90d82f
Erik Dubois <erik.dubois@gmail.com>
2021-11-14 17:00:28 +0100

    update

--- installation-of-i3/100-install-i3-gaps-next-v1.sh
+++ installation-of-i3/100-install-i3-gaps-next-v1.sh
@@ -9,21 +9,14 @@ set -e
 
 sudo apt install thunar -y
 sudo apt install gnome-terminal -y
-sudo apt install picom -y
-
-echo "##################################################"
-echo "Latest possible version of i3 with gaps"
-echo "##################################################"
+sudo apt install compton -y
 
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt update
-sudo apt install i3-gaps thunar gnome-terminal
+sudo apt install i3-gaps -y
 
-echo "If the keys can come in you are fine else build it yourself"
-echo "else run 120b - you need to install the dependencies from 100"
-echo
 echo
 echo "##################################################"
-echo "Latest possible version of i3 with gaps installed"
+echo "i3 with gaps installed"
 echo "##################################################"
 sleep 1
