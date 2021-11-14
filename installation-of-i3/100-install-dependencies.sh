219fb36e7711714855dd456af34b3f83d33e2db5
Erik Dubois <erik.dubois@gmail.com>
2021-11-14 16:17:20 +0100

    nothing needed

--- installation-of-i3/100-install-dependencies.sh
+++ installation-of-i3/100-install-dependencies.sh
@@ -8,14 +8,13 @@ set -e
 #
 ##################################################################################################################
 
-echo "#########################################"
-echo "dependencies for i3 with gaps"
-echo "#########################################"
+#echo "#########################################"
+#echo "dependencies for i3 with gaps"
+#echo "#########################################"
 
-# script 110
-sudo apt install -y autoconf
-sudo apt install -y automake
-sudo apt install -y build-essential
+# sudo apt install -y autoconf
+# sudo apt install -y automake
+# sudo apt install -y build-essential
 # sudo apt install -y libtool
 # sudo apt install -y xutils-dev xcb libxcb-composite0-dev
 # sudo apt install -y doxygen
@@ -24,11 +23,11 @@ sudo apt install -y build-essential
 #sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev 
 #sudo apt install -y libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
 
-echo
-echo
-echo "#########################################"
-echo "dependencies for i3 with gaps installed"
-echo "#########################################"
+#echo
+#echo
+#echo "#########################################"
+#echo "dependencies for i3 with gaps installed"
+#echo "#########################################"
 
 
 # echo "###############################"
@@ -52,3 +51,5 @@ echo "#########################################"
 # echo "Xcb-util-xrm installed"
 # echo "###############################"
 # sleep 3
+
+echo "nothing needed november 2021"
